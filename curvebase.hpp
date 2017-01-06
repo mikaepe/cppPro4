91774d5c6aed8d3b4a0e241ab583caa232974841
Mikael Persson <mikaepe@kth.se>
2017-01-06 10:26:19 +0100

    comments added

--- curvebase.hpp
+++ curvebase.hpp
@@ -15,13 +15,13 @@ class Curvebase {
     double b;
     double length;
 
-    
+    // Pure virtual, ("= 0"), derived classes must implement:
     virtual double xp(double p) = 0;	//parametrized by user
     virtual double yp(double p) = 0;	//parametrized by user
     virtual double dxp(double p) = 0;	//dx(p)/dp for arc length
     virtual double dyp(double p) = 0;	//dy(p)/dp for arc length
     
-    double integrate(double a, double b);
+    double integrate(double a, double b);	// TODO private?
     double newtonsolve(double p0, double s);
 
     double i2Simpson(double a, double b);
@@ -32,6 +32,10 @@ class Curvebase {
     Curvebase(); 			//default constructor
     virtual double x(double s); 	//parametrized by normalized arc length
     virtual double y(double s); 	//parametrized by normalized arc length
+
+    // TODO from slides 6 F_Inheritance.pdf page 30: 
+    // the destructor of abstract base class should always be virtual
+    // virtual ~Curvebase();
 
 };
 
