00802ba80ff57d1a533d4718b5e63a8d2ab3805d
Mikael Persson <mikaepe@kth.se>
2017-01-07 22:59:50 +0100

    fxcurve in testDomain.cpp

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CPPFLAGS=-Wall -g -std=c++11
 
 test1: test1.cpp matrix.cpp
-testDomain: testDomain.cpp domain.cpp curvebase.cpp xline.cpp yline.cpp xquad.cpp point.cpp gfctn.cpp matrix.cpp
+testDomain: testDomain.cpp domain.cpp curvebase.cpp xline.cpp yline.cpp fxcurve.cpp xquad.cpp point.cpp gfctn.cpp matrix.cpp
 pointMain: pointMain.cpp point.cpp
 testMatrix: testMatrix.cpp matrix.cpp
 
--- testDomain.cpp
+++ testDomain.cpp
@@ -7,12 +7,14 @@
 #include "xline.hpp"
 #include "yline.hpp"
 #include "xquad.hpp"
+#include "fxcurve.hpp"
 #include "domain.hpp"
 #include "gfctn.hpp"
 #include "point.hpp"		// f defined on points
 
 using namespace std;
 
+// function for testing the classes (as specified in lab instructions)
 inline double f(Point p) {
   return sin((p.X()*p.X()*0.01))*cos(p.X()*0.1) + p.Y();
 }
@@ -20,21 +22,25 @@ inline double f(Point p) {
 int main(int argc, char *argv[])
 {
 
-/*
-  xQuad a = xQuad(-.25,0,.25,-1,1);
-  yLine b = yLine(0,1,1);
-  xLine c = xLine(-1,1,1);
-  yLine d = yLine(0,1,-1);
-*/
 
+  /*
   shared_ptr<xQuad> a = make_shared<xQuad>(-.25,0,.25,-1,1);
   shared_ptr<yLine> b = make_shared<yLine>(0,1,1);
   shared_ptr<xLine> c = make_shared<xLine>(-1,1,1);
   shared_ptr<yLine> d = make_shared<yLine>(0,1,-1);
 
+  */
+
+  shared_ptr<fxCurve> a = make_shared<fxCurve>(-10.0,5.0);
+  shared_ptr<yLine> b = make_shared<yLine>(0.0,3.0,5.0);
+  shared_ptr<xLine> c = make_shared<xLine>(-10.0,5.0,3.0);
+  shared_ptr<yLine> d = make_shared<yLine>(0.0,3.0,-10.0);
+
+
+  
 
   shared_ptr<Domain> grid = make_shared<Domain>(a,b,c,d);
-  grid->grid_generation(6,3);
+  grid->grid_generation(50,20);
   cout << "x-size = " << grid->xsize() << endl;
   cout << "y-size = " << grid->ysize() << endl;
   grid->writeFile();
