efa8cd27fbd796ab1112e2eb9daf3abe9d91ddd2
Mikael Persson <mikaepe@kth.se>
2017-01-07 15:03:16 +0100

    grid plot with surf..

--- grid_plot.m
+++ grid_plot.m
@@ -6,3 +6,9 @@ m = fread(fID,1,'int');
 A = fread(fID,[(n+1)*(m+1) 2],'double');
 fclose(fID);
 plot(A(:,1),A(:,2),'*');
+
+X = vec2mat(A(:,1),m+1)';
+Y = vec2mat(A(:,2),m+1)';
+Z = sin(0.01*X.^2).*cos(0.1*X) + Y
+
+surf(X,Y,Z);
