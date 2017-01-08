017ab98ceef2dddd20ef26d78a39ba68c8587492
Hanna Hultin <hannahultin@live.se>
2017-01-08 12:57:49 +0100

    plotting fixed in matlab

--- grid_plot.m
+++ grid_plot.m
@@ -19,53 +19,53 @@ title('The function u')
 fID = fopen('DxOut.bin');
 n1 = fread(fID,1,'int');
 m1 = fread(fID,1,'int');
-Dx = fread(fID,[n1 m1],'double')';
+Dx = fread(fID,[m1 n1],'double')';
 fclose(fID);
 
 figure()
-surf(X,Y,Dx)
+surf(X,Y,Dx')
 title('First derivative w.r.t x')
 
 % Grid function for Dy
 fID = fopen('DyOut.bin');
 n1 = fread(fID,1,'int');
 m1 = fread(fID,1,'int');
-Dy = fread(fID,[n1 m1],'double')';
+Dy = fread(fID,[m1 n1],'double')';
 fclose(fID);
 
 figure()
-surf(X,Y,Dy)
+surf(X,Y,Dy')
 title('First derivative w.r.t y')
 
 % Grid function for DDx
 fID = fopen('DDxOut.bin');
 n1 = fread(fID,1,'int');
 m1 = fread(fID,1,'int');
-DDx = fread(fID,[n1 m1],'double')';
+DDx = fread(fID,[m1 n1],'double')';
 fclose(fID);
 
 figure()
-surf(X,Y,DDx)
+surf(X,Y,DDx')
 title('Second derivative w.r.t x')
 
 % Grid function for DDy
 fID = fopen('DDyOut.bin');
 n1 = fread(fID,1,'int');
 m1 = fread(fID,1,'int');
-DDy = fread(fID,[n1 m1],'double')';
+DDy = fread(fID,[m1 n1],'double')';
 fclose(fID);
 
 figure()
-surf(X,Y,DDy)
+surf(X,Y,DDy')
 title('Second derivative w.r.t y')
 
 % Grid function for Laplace operator
 fID = fopen('laplaceOut.bin');
 n1 = fread(fID,1,'int');
 m1 = fread(fID,1,'int');
-laplace = fread(fID,[n1 m1],'double')';
+laplace = fread(fID,[m1 n1],'double')';
 fclose(fID);
 
 figure()
-surf(X,Y,laplace)
+surf(X,Y,laplace')
 title('Laplace operator')
